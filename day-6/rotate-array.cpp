9ba24e8419e5ebea2e5485bb64a5fa58625691fc
yashsaxena750 <yashsaxena986@gmail.com>
2024-05-24 14:23:05 +0530

    using logic of k+i%length

--- day-6/rotate-array.cpp
+++ day-6/rotate-array.cpp
@@ -5,17 +5,32 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
 
-        while(k--)
-        {
-            int n = nums.size();
-            int temp = nums[n-1];
+        // while(k--)
+        // {
+        //     int n = nums.size();
+        //     int temp = nums[n-1];
         
-            for(int i=n-2;i>=0;i--)//0 1 2 3 4
-            {
+        //     for(int i=n-2;i>=0;i--)//0 1 2 3 4
+        //     {
             
-                nums[i+1] = nums[i];//7 6 5 4
-            }
-            nums[0] = temp;
+        //         nums[i+1] = nums[i];//7 6 5 4
+        //     }
+        //     nums[0] = temp;
+        // }
+
+
+        //using logic of k+i%length
+        int n = nums.size();
+        std::vector<int>res(n);
+        for(int i=0; i<n; i++)
+        {
+            res[(i+k)%n] = nums[ i ];
+        }
+        int i = 0;
+        for(int el:res)
+        {
+            nums[i] = el;
+            i++;
         }
 
     }
